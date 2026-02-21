5685d076a2238e4fa8d491d9f063186dd7d199e8
Especial2x4 <especial2x4@gmail.com>
2026-02-21 14:49:03 -0300

    modificando el mod de skycolor

--- mods/skycolor/init.lua
+++ mods/skycolor/init.lua
@@ -1,9 +1,31 @@
 
+local colors = {
+    "#39FF14",
+    "#00FF00",
+    "#0000FF",
+    "#FFFF00",
+    "#FF00FF",
+    "#00FFFF",
+    "#FFA500",
+    "#8000FF"
+}
+
 minetest.register_on_joinplayer(function(player)
-    minetest.after(1, function()
+
+    local function change_sky()
+        if not player or not player:is_player() then return end
+
+        local color = colors[math.random(#colors)]
+
         player:set_sky({
-            base_color = "#39FF14",
-            type = "plain"
+            type = "plain",
+            base_color = color
         })
-    end)
+
+        -- Próximo cambio entre 20 y 60 segundos
+        local next_time = math.random(20, 60)
+        minetest.after(next_time, change_sky)
+    end
+
+    change_sky()
 end)
